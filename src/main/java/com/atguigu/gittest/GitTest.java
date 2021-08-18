57530ae21911cd086a542fdcbe8925c354e6094a
ryan <ryanzl05@163.com>
2021-08-18 13:35:17 +0800

    pushi test

--- src/main/java/com/atguigu/gittest/GitTest.java
+++ src/main/java/com/atguigu/gittest/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.gittest;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
+        System.out.println("push test");
     }
 }
